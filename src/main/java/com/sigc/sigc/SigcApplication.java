78d5ca591b47eb5b90f63058c1c8e3bfcb06258f
cicatrizwp <pedrocmello.bsi@gmail.com>
2019-03-11 23:30:31 -0300

    adicionando base do projeto

---
+++ src/main/java/com/sigc/sigc/SigcApplication.java
@@ -0,0 +1,13 @@
+package com.sigc.sigc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigcApplication.class, args);
+	}
+
+}
